9c87398392ba7260f34948fc1a55459e52bc0ec6
vanshikagoel1 <91647992+vanshikagoel1@users.noreply.github.com>
2022-02-26 18:55:55 +0530

    Create Question 2
     Let’s say you are given a list saved in a variable:
    a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
    Write a Dart code that takes this list and makes a new list that has only the even elements of this list in it.

---
+++ Task 2/Question 2.dart
@@ -0,0 +1,16 @@
+void main() 
+{
+  List<int> a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+
+  int i = 0;
+  List<int> 1 = [];
+
+  for (var e in a) 
+  {
+    if (++i % 2 == 0) 
+    {
+      1.add(e);
+    }
+  }
+  print(1);
+}
